f2a6897b16b410da1ca3ec9bcd654b60d225fcb9
Bakyt Tursunbekov <btursunbekov@na.edu>
2023-09-12 21:08:03 -0500

    added my stores list

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,12 @@ use App\Traits\ModelCamelCase;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Collection;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property Store[]|Collection $stores
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, ModelCamelCase;
